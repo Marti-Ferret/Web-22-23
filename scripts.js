43079c04c263258e15a4d78eed4bdc414a28f8f0
Martí Ferret Cedó <73919571+Marti-Ferret@users.noreply.github.com>
2022-09-12 10:15:16 +0200

    Push2

---
+++ scripts.js
@@ -0,0 +1,24 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.estudis' , {
+        duration:4000,
+        origin: 'left',
+        distance: '200px',
+        delay:1000
+
+    });
+
+    sr.reveal('.treball', {
+        duration:4000,
+        origin: 'right',
+        distance: '200px',
+        delay:2000
+    })
+
+    sr.reveal('.titol' , {
+       distance:'500px',
+       duration:'3000',
+       opacity:0
+    });
+
+
